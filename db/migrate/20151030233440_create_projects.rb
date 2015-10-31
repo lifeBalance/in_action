fb643435bdd7608d9613138b1c6d1864012a9c6a
Javier Rodriguez <javierorodriguez@gmail.com>
2015-10-31 14:34:41 +0300

    'Create a new project' feature complete.

---
+++ db/migrate/20151030233440_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration
+  def up
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :projects
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # Excluding the /gems/ pattern from the otherwise chatty backtrace.
+  config.backtrace_exclusion_patterns << /gems/
 end
