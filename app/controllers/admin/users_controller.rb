04e38271950cd02ca0e8512bb9f71390ab1d0890
Javier Rodriguez <javierorodriguez@gmail.com>
2015-11-05 13:57:42 +0300

    Add feature for archiving users and protection against self-archiving

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,8 @@
 class Admin::UsersController < Admin::ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :archive]
 
   def index
-    @users = User.order(:email)
+    @users = User.excluding_archived.order(:email)
   end
 
   def show
@@ -31,7 +31,7 @@ class Admin::UsersController < Admin::ApplicationController
     if params[:user][:password].blank?
       params[:user].delete(:password)
     end
-    
+
     if @user.update(user_params)
       flash[:notice] = "User has been updated."
       redirect_to admin_users_path
@@ -39,6 +39,17 @@ class Admin::UsersController < Admin::ApplicationController
       flash.now[:alert] = "User has not been updated."
       render 'new'
     end
+  end
+
+  def archive
+    if @user == current_user
+      flash[:alert] = 'You cannot archive yourself!'
+    else
+      flash[:notice] = 'User has been archived.'
+      @user.archive
+    end
+
+    redirect_to admin_users_path
   end
 
   private
